8f0890444f8a6b2eb4ecdd7de0ea816ad8b96961
Nick Matvyeyev <mnasoft@gmail.com>
2022-09-22 08:53:57 +0300

    22 сен 2022 г.  5:53:57

--- docs/manifest.lisp
+++ docs/manifest.lisp
@@ -3,12 +3,12 @@
    :systems (:GASES)
    :documents ((:title "Gases"
 	        :authors ("Mykola Matvyeyev")
-	        :output-format (:TYPE :MULTI-HTML :TEMPLATE :GAMMA) 
-                :sources ("gases-reac.scr" "gases-molecule.scr"
-                          "gases-gas-dynamics.scr" "gases-db.scr"
-                          "gases-core.scr" "gases-const.scr"
-                          "gases-wet-air.scr" "gases-const-graph.scr"
-                          "gases-db-graph.scr" "gases-core-graph.scr"
-                          "gases-reac-graph.scr" "gases-gas-dynamics-graph.scr"
+	        :output-format (:TYPE :MULTI-HTML :TEMPLATE :GAMMA)
+                :sources ("gases-wet-air.scr" "gases-reac.scr"
+                          "gases-molecule.scr" "gases-gas-dynamics.scr"
+                          "gases-db.scr" "gases-core.scr" "gases-const.scr"
+                          "gases-const-graph.scr" "gases-db-graph.scr"
+                          "gases-core-graph.scr" "gases-reac-graph.scr"
+                          "gases-gas-dynamics-graph.scr"
                           "gases-molecule-graph.scr" "gases-wet-air-graph.scr")
                 )))
--- src/docs/docs.lisp
+++ src/docs/docs.lisp
@@ -49,26 +49,21 @@
 (defun make-all (&aux
                    (of (if (find (uiop:hostname)
                                  mnas-package:*intranet-hosts*
-                                 :test #'string=)
+                                 :test #'string= :key #'first)
                            '(:type :multi-html :template :gamma)
                            '(:type :multi-html :template :minima))))
-  "@b(Описание:) функция @b(make-all) служит для создания документации.
-
- Пакет документации формируется в каталоге
-~/public_html/Common-Lisp-Programs/gases.
-"
-  (mnas-package:make-html-path :gases)
-  (make-document)
-  (make-graphs)
-  (mnas-package:make-mainfest-lisp
-   '(:gases)
-   "Gases"
-   '("Mykola Matvyeyev")
-   (mnas-package:find-sources "gases")
-   :output-format of)
-  (codex:document :gases)
-  (make-graphs)
-  (mnas-package:copy-doc->public-html "gases")
-  (mnas-package:rsync-doc "gases"))
-
+  (let* ((sys-symbol :gases)
+         (sys-string (string-downcase (format nil "~a" sys-symbol))))
+    (mnas-package:make-html-path sys-symbol)
+    (make-document)
+    (mnas-package:make-mainfest-lisp `(,sys-symbol)
+                                     (string-capitalize sys-string)
+                                     '("Mykola Matvyeyev")
+                                     (mnas-package:find-sources sys-symbol)
+                                     :output-format of)
+    (codex:document sys-symbol)
+    (make-graphs)
+    (mnas-package:copy-doc->public-html sys-string)
+    (mnas-package:rsync-doc sys-string)
+    :make-all-finish))
 ;;;; (make-all)
